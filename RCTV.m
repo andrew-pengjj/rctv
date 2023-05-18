535c8948e17def451808c8a6d47ca126b910cddb
Jiangjun Peng <andrew.pengjj@gmail.com>
2023-05-18 15:02:14 +0800

    Update and rename MoG_RBTV.m to RCTV.m
    Change file name

--- RCTV.m
+++ RCTV.m
@@ -1,6 +1,6 @@
 %% solve the problem 
 %  Original Problem:
-%      min_{U,V,E}  beta*||W.E||_2 + lambda* ||S||_1 + tau*||U||_{TV}
+%      min_{U,V,E}  beta*||E||_2 + lambda* ||S||_1 + tau*||U||_{TV}
 %                  s.t. Y = UV' + E + S, rank(U)=r,  V'*V=I
 %  Auxiliary Problem:
 %      min_{U,V,E} 1/2*||W.E||_2 + lambda* ||S||_1 + tau*(\sum_i ||G_i||_1)
@@ -8,17 +8,29 @@
 %                       X = UV, rank(U)=r,  V'*V=I
 %                          ===============================                      
 %         D is difference operator
+%  Jiangjun Peng, October 2022
+%   If you have any question, please contact "andrew.pengjj@gmail.com" for
+%   more information
 %  ------------------------------------------------------------------------
-function [output_image,U,V,MPSNR,MSSIM] = MoG_RBTV(oriData3_noise, beta,lambda, tau, r, K)
+function [DenoisedHSI,U,V] = RCTV(Nhsi, beta,lambda, tau, r)
+%% Input Varaible
+%   Nhsi   --- Polluted HSI data
+%   beta   --- trade-off parameter of Gaussian noise
+%   lambda --- trade-off parameter of sparse noise
+%   rk     --- rank of needed-to-be recovered Tensor along mode-3
+%% Output Variable
+%   DenoisedHSI --- Denoised HSI
+%   U           --- Representative Coefficient Image(RCI)
+%   V           --- Orthogonal Basis
 %% Initializing admm variables
 tol     = 1e-5;
 maxIter = 100;
 rho     = 1.5;
 max_mu  = 1e6;
-[M,N,p] = size(oriData3_noise);
+[M,N,p] = size(Nhsi);
 D       = zeros(M*N,p) ;
 for i=1:p
-    bandp = oriData3_noise(:,:,i);
+    bandp = Nhsi(:,:,i);
     D(:,i)= bandp(:);
 end
 
@@ -35,13 +47,6 @@ end
 if nargin < 5
     r = 6;
 end
-if nargin < 6
-    K = 1;
-end
-
-Pk = ones(1,K)/K;
-Sigma = rand(1,K);
-
 
 % initialize
 Y = D;
@@ -49,7 +54,7 @@ norm_two = lansvd(Y, 1, 'L');
 norm_inf = norm( Y(:), inf);
 dual_norm = max(norm_two, norm_inf);
 Y = Y / dual_norm;
-mu = 1/norm_two
+mu = 1/norm_two;
 normD   = norm(D,'fro');
 %% FFT setting
 h               = M;
@@ -75,12 +80,9 @@ M2 = zeros(M*N*r,1);  % multiplier for Dy_U-G2
 W2 = ones(M*N*r,1);
 M3 = zeros([M*N,p]);  % multiplier for D-UV^T-E
 W3 = ones(M*N,p);
-Weight = ones(M*N,p);
 % main loop
 iter = 0;
 eps =1e-4;
-MPSNR = zeros(maxIter,1);
-MSSIM = zeros(maxIter,1);
 tic
 while iter<maxIter
     iter          = iter + 1;  
@@ -88,12 +90,6 @@ while iter<maxIter
     %% -Update G1,G2
     G1 = softthre_s(diff_x(U,sizeU)+M1/mu,tau(1)/mu,W1);
     G2 = softthre_s(diff_y(U,sizeU)+M2/mu,tau(2)/mu,W2);
-%     if iter >5
-%         tmp_max = max(abs(G1(:)));
-%         W1 = tmp_max./(abs(G1)+delta*tmp_max);
-%         tmp_max = max(abs(G2(:)));
-%         W2 = tmp_max./(abs(G2)+delta*tmp_max);
-%     end
     %% -Update U
     diffT_p  = diff_xT(G1-M1/mu,sizeU)+diff_yT(G2-M2/mu,sizeU);
     temp     = (D-E-S+M3/mu)*V;
@@ -105,15 +101,6 @@ while iter<maxIter
     V           = u*v';
     %% -Update E
     E = mu*(D-U*V'-S+M3/mu)/(2*beta+mu);
-%     if beta>100
-%         E = 0;
-%     else
-%         E = mu*(D-U*V'-S+M3/mu)/(2*beta+mu);
-% %     if mod(iter,4)==1
-% %         [Weight,~,Sigma,~,Pk] = MoGWeight(D-S,U,V,K,Pk,Sigma);
-% %     end
-% %     E = mu*(D-U*V'-S+M3/mu)./(2*beta*Weight+mu);
-%     end
     %% -Update S
     if lambda >100
         % 忽略掉高斯噪音影响
@@ -121,10 +108,6 @@ while iter<maxIter
     else
         S = softthre_s(D-U*V'-E+M3/mu,lambda/mu,W3);
     end
-    output_image = reshape(U*V',[M,N,p]);
-%     [psnr,ssim,~,~,~] = evaluate(clean_data,output_image,M,N);
-%     MPSNR(iter) = mean(psnr);
-%     MSSIM(iter) = mean(ssim);
     %% -Update Multiplier
     leq1 = diff_x(U,sizeU)-G1;
     leq2 = diff_y(U,sizeU)-G2;
@@ -146,7 +129,7 @@ while iter<maxIter
         mu = min(max_mu,mu*rho); 
     end 
 end
-output_image = reshape(U*V',[M,N,p]);
+DenoisedHSI = reshape(U*V',[M,N,p]);
 end
 function out=softthre_s(a,tau,w)
 out = sign(a).* max( abs(a) - tau*w, 0);
